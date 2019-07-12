ca1e52cad514260625a080e85b9478b61b4f6609
Makhabane Mipi <christopher.mipi@gmail.com>
2019-07-12 20:41:26 +0200

    API documentation

--- backend/src/main/java/za/co/operadev/demo/DemoApplication.java
+++ backend/src/main/java/za/co/operadev/demo/DemoApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -21,8 +25,22 @@ public class DemoApplication {
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("za.co.operadev.demo"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(getApiInfo());
+	}
+
+	private ApiInfo getApiInfo() {
+		return new ApiInfo(
+				"ToDos RESTful API",
+				"Capturing, Deleting and making items done",
+				"VERSION 1",
+				"TERMS OF SERVICE URL",
+				new Contact("Makhabane Mipi","https://mcmipi.xyz","makhabane@operadev.com"),
+				"APACHE LICENSE, VERSION 2.0",
+				"http://www.apache.org/licenses/LICENSE-2.0",
+				Collections.emptyList()
+		);
 	}
 }
